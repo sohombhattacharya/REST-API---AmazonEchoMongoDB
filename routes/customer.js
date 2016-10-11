fd3f53755cfc920156f1e15d2e0894b2f8b966ef
Sohom Bhattacharya <sohom95@yahoo.com>
2016-10-10 22:55:32 -0400

    changed position of res.json(response) in routes

--- routes/customer.js
+++ routes/customer.js
@@ -4,11 +4,14 @@ var Customer = require("../model/customer");
 function getCustomers(req, res){
     Customer.find({}, function(err, customers){
         var response; 
-        if (err)
+        if (err){
             response = { error: "could not get all customers"};
-        else    
+            res.json(response);
+        }
+        else{    
             response = customers;
-        res.json(response);
+            res.json(response);
+        }
     });     
 }
 
@@ -16,92 +19,120 @@ function postCustomer(req, res){
     var newCustomer = new Customer(req.body);
     newCustomer.save(function(err){
         var response; 
-        if (err)
+        if (err){
             response = { error: "could not add new customer"};
-        else
+            res.json(response);
+        }
+        else{
             response = { success: "added customer"};    
-        res.json(response); 
+            res.json(response);
+        }
     });
 }
 
 function getCustomer(req, res){
     Customer.find({_id: req.params.id}, function(err, customer){ 
         var response; 
-        if (err)
+        if (err){
             response = { error: "could not get customer"};
-        else{
-            if (customer.length == 0)
-                response = { error: "customer does not exist"}; 
-            else
-                response = customer[0]; 
+            res.json(response);
         }
-        res.json(response); 
+        else{
+            if (customer.length == 0){
+                response = { error: "customer does not exist"};
+                res.json(response);
+            }
+            else{
+                response = customer[0]; 
+                res.json(response);
+            }
+        }
     }); 
 }
 
 function deleteCustomer(req, res){
     Customer.findOneAndRemove({_id: req.params.id}, function(err) {
         var response; 
-        if (err)
+        if (err){
             response = { error: "could not find and delete customer"};
+            res.json(response);
+        }
         else{
             Customer.update({}, {$pull: {"friends": req.params.id}}, {multi: true}, function(err){
-                if (err)
+                if (err){
                     response = { error: "could not find and delete current customer as friends of others"};
-                else
+                    res.json(response);
+                }
+                else{
                     response = { success: "found and deleted customer"};     
+                    res.json(response);
+                }
             }); 
         }
-        res.json(response);
     }); 
 }
 
 function updateCustomer(req, res){ 
         var response; 
         Customer.find({_id: req.params.id}, function(err, customer){
-            if (err)
+            if (err){
                 response = { error: "could not find customer"};
+                res.json(response);
+            }
             else{
-                if (customer.length == 0)
+                if (customer.length == 0){
                     response = { error: "customer does not exist"}; 
+                    res.json(response);
+                }
                 else{
                     var updatedCustomer = Object.assign(customer[0], req.body); 
                     updatedCustomer.save(function(err, customer){
-                        if (err)
+                        if (err){
                             response = { error: "could not update customer"};
-                        else
+                            res.json(response);
+                        }
+                        else{
                             response = { success: "updated customer"};
+                            res.json(response);
+                        }
                     }); 
                 }
             }
-            res.json(response);
         }); 
 }
 
 function getFriends(req, res){
         Customer.find({_id: req.params.id}, function(err, customer){
             var response; 
-            if (err)
+            if (err){
                 response = { error: "could not get friends"};
+                res.json(response);
+            }
             else{
                 if (customer.length == 0){
                     response = { error: "customer does not exist"}; 
+                    res.json(response);
                 }
-                else
+                else{
                     response = customer[0].friends;
+                    res.json(response);
+                }
             }
-            res.json(response);
         }); 
 }
 
 function postFriend(req, res){
         Customer.find({_id: req.params.id}, function(err, customer){
             var response; 
-            if (err)
+            if (err){
                 response = { error: "could not get customer"};
+                res.json(response);
+            }
             else{
-                if (customer.length == 0)
+                if (customer.length == 0){
                     response = { error: "customer does not exist"}; 
+                    res.json(response);
+                }
                 else{
                     var i; 
                     var bool = 0; 
@@ -109,31 +140,39 @@ function postFriend(req, res){
                         if (customer[0].friends[i].id == req.params.friendID)
                             bool = 1; 
                     }
-                    if (bool == 1)
+                    if (bool == 1){
                         response = { error: "friend already exists!"};
+                        res.json(response);
+                    }
                     else{
-                        customer[0].friends.push(req.params.friendID); 
+                        customer[0].friends.push(req.params.friendID);
                         customer[0].save(function(err){
-                            if (err)
+                            if (err){
                                 response = { error: "could not add friends"};
-                            else
+                                res.json(response);
+                            }
+                            else{
                                 response = { success: "added friend"};
+                                res.json(response);
+                            }
                         });
                     }
                 }
             }
-            res.json(response);
         });
 }
 
 function deleteFriend(req, res){
             Customer.update({_id: req.params.id}, {$pull: {"friends": req.params.friendID}}, function(err){
                 var response; 
-                if (err)
+                if (err){
                     response = { error: "could not remove friend"};
-                else
+                    res.json(response);
+                }
+                else{
                     response = { success: "removed friend"}; 
-                res.json(response); 
+                    res.json(response);
+                }
             });     
 }
 
--- test/customer.js
+++ test/customer.js
@@ -9,3 +9,23 @@ var expect = chai.expect();
 chai.use(chaiHttp); 
 
    
+describe("Customer", () => {
+    beforeEach((done) => {
+        customer.remove({}, (err) => { 
+           done();         
+        });     
+    });
+  describe('/GET customer', () => {
+      it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/api/customer')
+            .end((err, res) => {
+                res.should.have.status(4);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+              
+            });
+          done();
+      });
+  });
+});
